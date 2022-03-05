21cd98e12856f2e2be03e7f5dc9afe600f6eae65
Joe <jw051098@gmail.com>
2022-03-05 15:31:19 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Bio from './Bio';
+
+function App() {
+  return (
+    <div>
+      <h1 className="welcome">
+        Welcome to my website!
+      </h1>
+      <Bio />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Bio.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Bio(){
+
+    return(
+        <p>
+            Hi, and thanks for finding my website! My name is Joe Walsh, and I'm an aspiring
+            software engineer. I'm always excited to learn something new and so I'm looking
+            for the opportunity to get some hands on experince.
+        </p>
+    )
+
+}
+
+export default Bio;
