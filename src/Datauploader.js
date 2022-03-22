e533b3817ac612d0512fca0987a7d554b98d7dcc
Joe <jw051098@gmail.com>
2022-03-21 22:02:16 -0400

    Parse through directory files, gives general information about each file

--- src/Datauploader.js
+++ src/Datauploader.js
@@ -1,27 +1,34 @@
 const csv = require('csv-parser');
-const fs = require('fs')
+const fs = require('fs');
 
-const fileArray = fs.readdirSync('./data')
-let sum =0
-let highest = -1
-let lowest = 99999
-let count =0
+const fileArray = fs.readdirSync('./raw_data')
 
+fileArray.forEach(fileName => {
+    
+    let sum =0
+    let highest = -1
+    let lowest = 999999
+    let count =0
 
-fs.createReadStream('./data/20220314-0705.csv')
-    .pipe(csv())
-    .on('data', (row) => {
-    sum = sum + parseInt(row.score)
-    count+=1;
-    if(parseInt(row.score) > highest){
-        highest = parseInt(row.score)
-    }
+    currentFile = './raw_data/'+fileName;
 
-    if(parseInt(row.score) < lowest)
-        lowest = parseInt(row.score)
-    })
-    .on('end', () => {
-    console.log('Average karma: ' + parseInt(sum/count))
-    console.log('Highest: ' + highest)
-    console.log('Lowest: ' + lowest)
-    });
+    fs.createReadStream(currentFile)
+        .pipe(csv())
+        .on('data', (row) => {
+        sum = sum + parseInt(row.score)
+        count+=1;
+        if(parseInt(row.score) > highest){
+            highest = parseInt(row.score)
+        }
+
+        if(parseInt(row.score) < lowest)
+            lowest = parseInt(row.score)
+        })
+        .on('end', () => {
+            console.log("File inormation for: " + fileName)
+            console.log('Average karma: ' + parseInt(sum/count))
+            console.log('Highest: ' + highest)
+            console.log('Lowest: ' + lowest)
+            console.log('\n')
+        });
+});
