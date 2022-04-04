e6b9ed9ac9c34811d0d552ce52ef64108fccd722
Joe <jw051098@gmail.com>
2022-04-04 00:25:12 -0400

    Structured JSON object for each post. Generated timestamp from filename.

--- src/Datauploader.js
+++ src/Datauploader.js
@@ -1,34 +1,46 @@
-const csv = require('csv-parser');
 const fs = require('fs');
+const csv = require('csv-parser');
+const { exit } = require('process');
 
 const fileArray = fs.readdirSync('./raw_data')
+const arr = [1]
+let post_ids = []
 
-fileArray.forEach(fileName => {
-    
-    let sum =0
-    let highest = -1
-    let lowest = 999999
-    let count =0
 
-    currentFile = './raw_data/'+fileName;
+
+arr.forEach(fileName => {
+    //currentFile = './raw_data/'+fileName;
+    let currentFile = '../20220314-0935.csv'
+    let timestamp = currentFile.slice(3,-4);
 
     fs.createReadStream(currentFile)
         .pipe(csv())
-        .on('data', (row) => {
-        sum = sum + parseInt(row.score)
-        count+=1;
-        if(parseInt(row.score) > highest){
-            highest = parseInt(row.score)
-        }
-
-        if(parseInt(row.score) < lowest)
-            lowest = parseInt(row.score)
+        .on('data', (data) =>{
+            post_id = data.id;
+            post_title = data.title;
+            post_author = data.author;
+            post_score = data.score;
+            post_sub = data.subreddit;
+            post_comments= data.num_comments;
+            post_link = data.permalink;
+            post_isNSFW = data.over_18;
+            let JSONobject = {
+                id : post_id,
+                details : {
+                    title : post_title,
+                    author : post_author,
+                    subreddit: post_sub,
+                    link : post_link,
+                    isNSFW : post_isNSFW,
+                    score : {post_score},
+                    num_comments : {post_comments}
+                }
+            }
+            console.log(JSONobject.details.score.post_score);
+            
         })
         .on('end', () => {
-            console.log("File inormation for: " + fileName)
-            console.log('Average karma: ' + parseInt(sum/count))
-            console.log('Highest: ' + highest)
-            console.log('Lowest: ' + lowest)
-            console.log('\n')
-        });
+            ;
+        })
+
 });
